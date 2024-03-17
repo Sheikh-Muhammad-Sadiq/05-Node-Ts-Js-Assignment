2a577c23e0857e0d857015c9cd061e9961ab4c5d
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:04:44 -0700

    Add files via upload
    Q 05 Node.js of 45 Assignment

---
+++ Q5.js
@@ -0,0 +1,6 @@
+"use strict";
+// 5. Famous Quote: Repeat Exercise 4, but this time store the famous Person's name in a variable called 
+// famous_person. Then compose yoour message and store in it a new variable called Message. Print your Message.
+let famous_person = "Hazrat ALI";
+let message = "Accept the Apology, Even if it is Not Sincer";
+console.log(`${famous_person} Once Said,“  ${message} ”`);
---
+++ Q5.ts
@@ -0,0 +1,8 @@
+// 5. Famous Quote: Repeat Exercise 4, but this time store the famous Person's name in a variable called 
+// famous_person. Then compose yoour message and store in it a new variable called Message. Print your Message.
+
+let famous_person: string = "Hazrat ALI"
+
+let message: string = "Accept the Apology, Even if it is Not Sincer"
+
+console.log(`${famous_person} Once Said,“  ${message} ”`)
